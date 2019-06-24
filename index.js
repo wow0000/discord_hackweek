a6536870c504732234bb7ab816ebdb03edeabdd1
wow <11897160+wow0000@users.noreply.github.com>
2019-06-24 20:25:05 +0200

    Removed twitter support

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var Twitter = require('twitter');
 var config = require('./config.json');
-
-var tw_client = new Twitter({
-  consumer_key: config.twitter.consumer_key,
-  consumer_secret: config.twitter.consumer_secret,
-  access_token_key: config.twitter.access_token_key,
-  access_token_secret: config.twitter.access_token_secret
-});
-
-var tw_params = {screen_name: 'nodejs'};
 
 client.on('ready', () => {
   console.log(`rdy ${client.user.tag}!`);
